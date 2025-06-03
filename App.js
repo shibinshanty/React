fb49c30f3ade2fded0ab3af9fe561afc6c1fce47
shibinshanty <shibinshanty612000@gmail.com>
2025-06-03 13:43:53 +0530

    Component Composition  'adding a functional component into another functional Component'

--- App.js
+++ App.js
@@ -6,13 +6,14 @@
 // jsx => React.CreateElement => ReactElement js Object=> Html Element
 
 //React Element
-const jsxHeading=<h1 id='heading'>Namaste React Using JSX</h1>
-
-
+const TitleComponent=()=>(
+<h1 id='heading'>Namaste React Using JSX</h1>
+);
 //React Functional Component
 
 const HeadingComponent=()=>(
 <div id='top'>
+<TitleComponent/>
 <h1 className="heading">Namaste React in Functional Component</h1>
 </div>
 )
