433f1d5b8539d0c3a4e4880125f4c7672553c210
shibinshanty <shibinshanty612000@gmail.com>
2025-06-03 16:47:41 +0530

    adding react elements and varables into function with curly braces

--- App.js
+++ App.js
@@ -9,11 +9,21 @@
 const TitleComponent=()=>(
 <h1 id='heading'>Namaste React Using JSX</h1>
 );
+
+// add react element into the functions with curly braces
+const heading=(<h1>Namaste React</h1>)
+
+// add variables into function with curly braces
+const number=10000
+
+
 //React Functional Component
 
 //Component composition
 const HeadingComponent=()=>(
 <div id='top'>
+    {heading}
+    {10000}
 <TitleComponent/>   
 <h1 className="heading">Namaste React in Functional Component</h1>
 </div>
